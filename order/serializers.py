817d05a26fa1e6b20d23dbb334d2d9c28fd14157
Sundayz <sundayz.hunter@proton.me>
2025-01-20 16:07:54 +0100

    fix(order): handle nested items during order creation

--- order/serializers.py
+++ order/serializers.py
@@ -1,14 +1,12 @@
 from rest_framework import serializers
 
 from order.models import OrderItem, Order
+from product.models import Product
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(source='product.name')
-    product_price = serializers.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        source='product.price')
+    product_price = serializers.DecimalField(max_digits=10, decimal_places=2, source='product.price', read_only=True)
 
     class Meta:
         model = OrderItem
@@ -22,7 +20,8 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True, read_only=True)
+    order_id = serializers.UUIDField(read_only=True)
+    items = OrderItemSerializer(many=True)
     total_price = serializers.SerializerMethodField(method_name='total')
 
     class Meta:
@@ -45,3 +44,20 @@ class OrderSerializer(serializers.ModelSerializer):
     def total(self, obj):
         order_items = obj.items.all()
         return sum(order_item.item_subtotal for order_item in order_items)
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        order = Order.objects.create(**validated_data)
+
+        for item_data in items_data:
+            product_name = item_data['product']['name']
+            try:
+                product = Product.objects.get(name=product_name)
+            except Product.DoesNotExist:
+                raise serializers.ValidationError(
+                    f"Product with name '{product_name}' does not exist."
+                )
+
+            OrderItem.objects.create(order=order, product=product, quantity=item_data.get('quantity'))
+
+        return order
