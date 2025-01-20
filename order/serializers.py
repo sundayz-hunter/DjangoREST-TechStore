316369f4f5683e0c5e3408405c09cb2c9af52b1c
Sundayz <sundayz.hunter@proton.me>
2025-01-20 17:24:27 +0100

    fix(order): handle nested items during order update

--- order/serializers.py
+++ order/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from order.models import OrderItem, Order
@@ -48,17 +49,66 @@ class OrderSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         items_data = validated_data.pop('items')
-        order = Order.objects.create(**validated_data)
 
-        for item_data in items_data:
-            product_name = item_data['product']['name']
-            try:
-                product = Product.objects.get(name=product_name)
-            except Product.DoesNotExist:
-                raise serializers.ValidationError(
-                    f"Product with name '{product_name}' does not exist."
-                )
+        with transaction.atomic():
+            order = Order.objects.create(**validated_data)
 
-            OrderItem.objects.create(order=order, product=product, quantity=item_data.get('quantity'))
+            for item_data in items_data:
+                product_name = item_data['product']['name']
+                try:
+                    product = Product.objects.get(name=product_name)
+                except Product.DoesNotExist:
+                    raise serializers.ValidationError(
+                        f"Product with name '{product_name}' does not exist."
+                    )
+
+                OrderItem.objects.create(order=order, product=product, quantity=item_data.get('quantity'))
 
         return order
+
+    def update(self, instance, validated_data):
+        items_data = validated_data.pop('items')
+
+        with transaction.atomic():
+            instance = super().update(instance, validated_data) # Update order status (Confirmed, Cancelled)
+
+            # Update Items
+            existing_items = {item.product.id: item for item in instance.items.all()}
+            new_items_data = []
+
+            for item_data in items_data:
+                product_name = item_data['product']['name']
+                try:
+                    product = Product.objects.get(name=product_name)
+                except Product.DoesNotExist:
+                    raise serializers.ValidationError(
+                        f"Product with name '{product_name}' does not exist."
+                    )
+
+                if product.id in existing_items:
+                    # Update quantity of existing item
+                    existing_item = existing_items[product.id]
+                    existing_item.quantity = item_data.get('quantity', existing_item.quantity)
+                    existing_item.save()
+                else:
+                    # Create new item added in the order
+                    new_items_data.append(OrderItem(
+                        order=instance,
+                        product=product,
+                        quantity=item_data.get('quantity', 1)
+                    ))
+
+            # Deleting items not included in the new list only if PUT request method
+            if self.partial == False:
+                items_to_delete = [
+                    item for product_id, item in existing_items.items()
+                    if product_id not in [Product.objects.get(name=item['product']['name']).id for item in items_data]
+                ]
+                for item in items_to_delete:
+                    item.delete()
+
+                # Creating new items
+                OrderItem.objects.bulk_create(new_items_data)
+
+
+        return instance
